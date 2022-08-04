bd7babb2bf10dda3b5b141d042ee1553dd4f6975
Kauã Amaral <kauaamaral10@gmail.com>
2022-08-03 22:06:32 -0300

    Repositorio Criado Para exemplo Html Exemplo de cshtml criado em sala de aula

---
+++ Entra21.CSharp.ClinicaVeterinaria/Entra21.CSharp.ClinicaVeterinaria.Aplicacao/Controllers/RacaController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Entra21.CSharp.ClinicaVeterinaria.Aplicacao.Controllers
+{
+    // Dois pontos Heranca(mais para frente)
+    public class RacaController : Controller
+    {
+        /// <summary>
+        /// Endpoint que permite listar todas as racas
+        /// </summary>
+        /// <returns>Retorna a pagina html com as racas</returns>
+        [Route("/raca")]
+        [HttpGet]
+        public IActionResult ObterTodos()
+        {
+            return View("Index");
+        }
+
+        [Route("/raca/cadastrar")]
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [Route("/raca/registrar")]
+        [HttpGet]
+        public IActionResult Registrar(
+            [FromQuery] string nome,
+            [FromQuery] string especie)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
