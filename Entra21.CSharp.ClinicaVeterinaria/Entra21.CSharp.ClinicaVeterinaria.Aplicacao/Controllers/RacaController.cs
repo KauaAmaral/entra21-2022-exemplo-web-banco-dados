e3fcfcf39f586d5dee17715d7bf768fd8089caa0
Kauã Amaral <kauaamaral10@gmail.com>
2022-08-08 22:11:47 -0300

    Adicionado mais coisas no exemplo adicionado mais codigo em sala de aula

--- Entra21.CSharp.ClinicaVeterinaria/Entra21.CSharp.ClinicaVeterinaria.Aplicacao/Controllers/RacaController.cs
+++ Entra21.CSharp.ClinicaVeterinaria/Entra21.CSharp.ClinicaVeterinaria.Aplicacao/Controllers/RacaController.cs
@@ -1,10 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Entra21.CSharp.ClinicaVeterinaria.Repositorio.BancoDados;
+using Entra21.CSharp.ClinicaVeterinaria.Servico;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Entra21.CSharp.ClinicaVeterinaria.Aplicacao.Controllers
 {
     // Dois pontos Heranca(mais para frente)
     public class RacaController : Controller
     {
+        private readonly RacaServico _racaServico;
+
+        // Construtor: objetivo construir o objeto de RacaController, com o minimo necessario para o funcionamento correto
+        public RacaController(ClinicaVeterinariaContexto contexto)
+        {
+            _racaServico = new RacaServico(contexto);
+        }
+
         /// <summary>
         /// Endpoint que permite listar todas as racas
         /// </summary>
@@ -29,6 +39,8 @@ namespace Entra21.CSharp.ClinicaVeterinaria.Aplicacao.Controllers
             [FromQuery] string nome,
             [FromQuery] string especie)
         {
+            _racaServico.Cadastrar(nome, especie);
+
             return RedirectToAction("Index");
         }
     }
---
+++ Entra21.CSharp.ClinicaVeterinaria/Repositorio/Entidades/Raca.cs
@@ -0,0 +1,10 @@
+﻿namespace Entra21.CSharp.ClinicaVeterinaria.Repositorio.Entidades
+{
+    // Entidade: sao representacoes das tabelas para classes de objetos
+    public class Raca
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Especie { get; set; }
+    }
+}
