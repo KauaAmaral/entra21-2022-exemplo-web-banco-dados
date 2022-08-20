8be6405ee9ad76dc5064c8da24ee491e5f141131
Kauã Amaral <kauaamaral10@gmail.com>
2022-08-19 21:58:42 -0300

    Ajuste comentarios de code review

--- Entra21.CSharp.ClinicaVeterinaria/Servico/ViewModels/RacaViewModel.cs
+++ Entra21.CSharp.ClinicaVeterinaria/Servico/ViewModels/RacaViewModel.cs
@@ -4,7 +4,6 @@ namespace Entra21.CSharp.ClinicaVeterinaria.Servico.ViewModels
 {
     public class RacaViewModel
     {
-        //[Display(Name = "Nome")]
         [Display(Name = nameof(Nome))]
         [Required(ErrorMessage = "{0} deve ser preenchido")]
         [MinLength(4, ErrorMessage = "{0} deve conter no mínimo {1} caracteres")]
